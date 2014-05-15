eb1f650e034f98a4c37544acbcb015f909cb107d
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2014-05-15 14:27:31 +0200

    Create doc.go with deletion warning.

---
+++ doc.go
@@ -0,0 +1,6 @@
+
+// package bitcoin is marked for deletion.
+// WARNING! WARNING!
+// This package is marked for deletion and should not be used!
+// Please use github.com/conformal/btcrpcclient instead.
+package bitcoin
